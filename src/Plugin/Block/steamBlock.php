22d5e09d224a4679080e9008cb58eaf73383659f
Nicole Fonseca <nickpontoc@gmail.com>
2023-11-22 21:35:47 -0300

    update code block to accept more than one template

--- src/Plugin/Block/steamBlock.php
+++ src/Plugin/Block/steamBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\steam_module\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Render\RendererInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Hello' Block.
@@ -16,13 +19,33 @@ use Drupal\Core\Block\BlockBase;
 class SteamBlock extends BlockBase {
 
   /**
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+  
+  public function __construct(RendererInterface $renderer) {
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('renderer')
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function build() {
     return [
       '#attached' => [
         'library' => [
-          'commerce_cart_flyout/flyout',
+          'steam_module/steam_block',
         ],
         'drupalSettings' => [
           'steamBlock' => [
@@ -34,6 +57,22 @@ class SteamBlock extends BlockBase {
       ],
       '#markup' => Markup::create('<div class="steam-block"></div>'),
     ];
+  }
+
+  /**
+   * Renders a template.
+   *
+   * @param string $hook
+   *   The theme hook.
+   *
+   * @return string
+   *   The rendered template.
+   */
+  protected function renderTemplate($hook) {
+    return $this->renderer->executeInRenderContext(new RenderContext(), function () use ($hook) {
+      $build = ['#theme' => $hook];
+      return $this->renderer->render($build);
+    });
   }
 
 }
